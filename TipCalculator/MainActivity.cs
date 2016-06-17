90689c0aabe1c63e9228f6d5e9c982d104670428
joedaouddotcom <joedaoud@outlook.com>
2016-06-17 14:59:46 -0700

    Add code behind

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -1,18 +1,43 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Widget;
+using System;
 
 namespace TipCalculator
 {
-    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "jddc Tip Calculator", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        EditText inputBill;
+        Button   calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            
+            inputBill       = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip       = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal     = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnClick;
+
+        }
+
+        void OnClick(object sender, EventArgs e)
+        {
+            string s = inputBill.Text;
+
+            var bill = double.Parse(s);
+
+            double tip = bill*0.15;
+            double total = bill + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
         }
     }
 }
